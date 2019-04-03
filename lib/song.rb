d3d92dc76fce4f0ec71c246472244835cc840b0d
Mark Kanko <mkanko66@gmail.com>
2019-04-02 23:47:58 -0700

    completed advanced-class-methods-lab

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
-  @@all = []
 
+  @@all = []
+ 
   def self.all
     @@all
   end
@@ -10,4 +13,60 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    #@@all << song 
+    song.save 
+    song  
+  end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    #binding.pry  
+    song 
+  end
+  
+  def self.create_by_name(name)
+    song = self.create 
+    song.name = name 
+    song  
+  end 
+
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name} 
+    #binding.pry 
+  end 
+
+  def self.find_or_create_by_name(name) 
+   self.find_by_name(name) || self.create_by_name(name)
+  end 
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+    #binding.pry 
+  end
+  
+  def self.new_from_filename(filename)
+    song = self.new 
+    song.name = filename.split("- ")[1].chomp('.mp3')
+    #binding.pry
+    song.artist_name = filename.split(" -")[0]
+    song  
+    #binding.pry  
+  end
+  
+  def self.create_from_filename(filename)
+    song = self.create 
+    song.name = filename.split("- ")[1].chomp('.mp3')
+    song.artist_name = filename.split(" -")[0]
+    #binding.pry 
+    song  
+  end
+  
+  def self.destroy_all
+    self.all.clear 
+  end 
+
+  
 end
