ed74452511b2fc31d359778e4197d49203281e30
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-04 03:19:03 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,65 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{ |song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+    @@all.sort_by { |song| song.name}
+  end
+  
+  def self.new_from_filename(name)
+    split_array_with_ext = name.split(" - ")
+    split_array = split_array_with_ext.map do |dunno|
+      dunno.split(".").delete_if {|x| x == "mp3"}.join    
+    end
+    name = split_array[1]
+    artist_name = split_array[0]
+    song = self.new
+    song.name =name
+    song.artist_name = artist_name
+    song
+  end
+  
+  def self.create_from_filename(name)
+    split_array_with_ext = name.split(" - ")
+    split_array = split_array_with_ext.map do |dunno|
+      dunno.split(".").delete_if {|x| x == "mp3"}.join    
+    end
+    name = split_array[1]
+    artist_name = split_array[0]
+    song = self.new
+    song.name =name
+    song.artist_name = artist_name
+    song.save
+    song
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
 end
