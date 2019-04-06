d456c52c2f267fcc24356e3f5c2c5dc9c057a33b
Leeya Davis <leeya463@gmail.com>
2019-04-06 03:04:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +10,77 @@ class Song
   def save
     self.class.all << self
   end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song.save
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song.save
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find do |song|
+      song.name == song_name
+    end
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if song = self.find_by_name(song_name)
+      song
+    else
+      song = self.create_by_name(song_name)
+      song
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort do |song1, song2|
+       song1.name <=> song2.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    file = filename.split("-")
+
+    artist = file[0].gsub(" ", "")
+    song_name = file[1].lstrip.gsub(/\.mp3/, "")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist
+    song
+  end
+
+  def self.create_from_filename(filename)
+    file = filename.split("-")
+
+    artist = file[0].gsub(" ", "")
+    song_name = file[1].lstrip.gsub(/\.mp3/, "")
+
+    song = self.new
+    song.artist_name = artist
+    song.name = song_name
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+
 
 end
